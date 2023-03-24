e5abb185eb06449d5fcc8a90ddf75389e4ee87a1
Bowen Bayuriyadi <b.bayuriyadi@gmail.com>
2023-03-24 23:22:13 +0700

    first commit

---
+++ Airflow/dags/to_postgresql.py
@@ -0,0 +1,294 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+default_args = {
+    'owner': 'Bowen66',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+
+with DAG(
+    dag_id='to_posgrest_v4.9',
+    default_args=default_args,
+    description='Raw to postgress connection',
+    start_date=datetime(2023, 3, 18, 2),
+    schedule_interval=None
+) as dag:
+    task1 = PostgresOperator(
+        task_id='create_postgres_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            create table if not exists dim_temperature (
+                date DATE PRIMARY KEY,
+                min DECIMAL(5, 2),
+                max DECIMAL(5, 2),
+                normal_min DECIMAL(5, 2),
+                normal_max DECIMAL(5, 2),
+                mean DECIMAL(5, 2),
+                status_temp int
+            ) 
+        """
+    )
+
+    task2 = PostgresOperator(
+        task_id='read_and_load_temperature',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TEMPORARY TABLE dim_temp_temperature (
+                date DATE PRIMARY KEY,
+                min DECIMAL(5, 2),
+                max DECIMAL(5, 2),
+                normal_min DECIMAL(5, 2),
+                normal_max DECIMAL(5, 2),
+                mean DECIMAL(5, 2),
+                status_temp int
+            );
+
+            COPY dim_temp_temperature FROM '/Users/Bowen/Downloads/Final_Project/data/output/transformation-temperature-LasVegas' DELIMITER ',' CSV HEADER;
+            INSERT INTO dim_temperature (date, min, max, normal_min, normal_max, mean, status_temp)
+            SELECT date, min, max, normal_min, normal_max, mean, status_temp
+            FROM dim_temp_temperature
+            ON CONFLICT (date) DO NOTHING;
+        """
+    )
+
+    task3 = PostgresOperator(
+        task_id='create_postgres_table_precipitation',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            create table if not exists dim_precipitation (
+                date DATE PRIMARY KEY,
+                precipitation DECIMAL(5, 2),
+                precipitation_normal DECIMAL(5, 2),
+                status_preci int
+            )
+        """
+    )
+
+    task4 = PostgresOperator(
+        task_id='read_and_load_precipitation',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TEMPORARY TABLE dim_temp_precipitation (
+                date DATE PRIMARY KEY,
+                precipitation DECIMAL(5, 2),
+                precipitation_normal DECIMAL(5, 2),
+                status_preci int
+            );
+
+            COPY dim_temp_precipitation FROM '/Users/Bowen/Downloads/Final_Project/data/output/transformation-precipitation-LasVegas.csv' DELIMITER ',' CSV HEADER;
+            INSERT INTO dim_precipitation (date, precipitation, precipitation_normal, status_preci)
+            SELECT date, precipitation, precipitation_normal, status_preci
+            FROM dim_temp_precipitation
+            ON CONFLICT (date) DO NOTHING;
+        """
+    )
+
+    task5 = PostgresOperator(
+        task_id='create_postgres_table_business',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            create table if not exists dim_business (
+                business_id text PRIMARY KEY,
+                name text,
+                address text,
+                city text,
+                state text,
+                postal_code text,
+                latitude float,
+                longitude float,
+                stars float,
+                review_count int,
+                is_open int,
+                attributes text,
+                categories text,
+                hours text
+            ) 
+        """
+    )
+
+    task6 = PostgresOperator(
+        task_id='read_and_load_business',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TEMPORARY TABLE dim_temp_business (
+                business_id text PRIMARY KEY,
+                name text,
+                address text,
+                city text,
+                state text,
+                postal_code text,
+                latitude float,
+                longitude float,
+                stars float,
+                review_count int,
+                is_open int,
+                attributes text,
+                categories text,
+                hours text
+            );
+
+            COPY dim_temp_business FROM '/Users/Bowen/Downloads/Final_Project/data/output/business_restaurant.csv' DELIMITER ',' CSV HEADER;
+            INSERT INTO dim_business (business_id, name, address, city, state, postal_code, latitude, stars, review_count, is_open, attributes, categories, hours)
+            SELECT business_id, name, address, city, state, postal_code, latitude, stars, review_count, is_open, attributes, categories, hours
+            FROM dim_temp_business
+            ON CONFLICT (business_id) DO NOTHING;
+        """
+    )
+
+    task7 = PostgresOperator(
+        task_id='create_postgres_table_user',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            create table if not exists dim_user1 (
+                user_id text PRIMARY KEY,
+                name text,
+                review_count text,
+                useful int,
+                funny int,
+                cool int,
+                elite text,
+                friends text,
+                fans text,
+                average_stars float,
+                yelping_since_values date
+            ) 
+        """
+    )
+
+    task8 = PostgresOperator(
+        task_id='read_and_load_user',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TEMPORARY TABLE dim_temp_user1 (
+                user_id text PRIMARY KEY,
+                name text,
+                review_count text,
+                useful int,
+                funny int,
+                cool int,
+                elite text,
+                friends text,
+                fans text,
+                average_stars float,
+                yelping_since_values date
+            );
+
+            COPY dim_temp_user1 FROM '/Users/Bowen/Downloads/Final_Project/data/output/user_restaurant.csv' DELIMITER ',' CSV HEADER;
+            INSERT INTO dim_user1 (user_id, name, review_count, useful, funny, cool, elite, friends, fans, average_stars, yelping_since_values)
+            SELECT user_id, name, review_count, useful, funny, cool, elite, friends, fans, average_stars, yelping_since_values
+            FROM dim_temp_user1
+            ON CONFLICT (user_id) DO NOTHING;
+        """
+    )
+
+    task9 = PostgresOperator(
+        task_id='create_postgres_table_checkin',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            create table if not exists dim_checkin (
+                business_id text,
+                date_values date PRIMARY KEY
+            ) 
+        """
+    )
+
+    task10 = PostgresOperator(
+        task_id='read_and_load_checkin',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TEMPORARY TABLE dim_temp_checkin (
+                business_id text,
+                date_values date PRIMARY KEY
+            );
+
+            COPY dim_temp_checkin FROM '/Users/Bowen/Downloads/Final_Project/data/output/checkin_restaurant1.csv' DELIMITER ',' CSV HEADER;
+            INSERT INTO dim_checkin (business_id, date_values)
+            SELECT DISTINCT ON (date_values) business_id, date_values
+            FROM dim_temp_checkin
+            ORDER BY date_values
+            ON CONFLICT (date_values) DO NOTHING;
+        """
+    )
+
+    task11 = PostgresOperator(
+        task_id='create_postgres_table_tip',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            create table if not exists dim_tip (
+                user_id text not null PRIMARY KEY,
+                business_id text not null,
+                text text not null,
+                compliment_count int not null,
+                date_values date not null
+            ) 
+        """
+    )
+
+    task12 = PostgresOperator(
+        task_id='read_and_load_tip',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TEMPORARY TABLE dim_temp_tip (
+                user_id text not null,
+                business_id text not null,
+                text text not null,
+                compliment_count int not null,
+                date_values date not null
+            );
+
+            COPY dim_temp_tip FROM '/Users/Bowen/Downloads/Final_Project/data/output/tip_restaurant.csv' DELIMITER ',' CSV HEADER NULL 'NA';
+            INSERT INTO dim_tip (user_id, business_id, text, compliment_count, date_values)
+            SELECT user_id, business_id, text, compliment_count, date_values
+            FROM dim_temp_tip
+            ON CONFLICT (user_id) DO NOTHING;
+        """
+    )
+
+    task13 = PostgresOperator(
+        task_id='create_postgres_table_review',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            create table if not exists fact_review (
+                review_id text PRIMARY KEY,
+                user_id text,
+                business_id text,
+                stars int,
+                useful int,
+                funny int,
+                cool int,
+                text text,
+                date_values date
+            ) 
+        """
+    )
+
+    task14 = PostgresOperator(
+        task_id='read_and_load_review',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TEMPORARY TABLE fact_temp_review (
+                review_id text not null PRIMARY KEY,
+                user_id text not null,
+                business_id text not null,
+                stars int not null,
+                useful int not null,
+                funny int not null,
+                cool int not null,
+                text text not null,
+                date_values date not null
+            );
+
+            COPY fact_temp_review FROM '/Users/Bowen/Downloads/Final_Project/data/output/review_restaurant.csv' DELIMITER ',' CSV HEADER NULL 'NA';
+            INSERT INTO fact_review (review_id, user_id, business_id, stars, useful, funny, cool, text, date_values)
+            SELECT review_id, user_id, business_id, stars, useful, funny, cool, text, date_values
+            FROM fact_temp_review;
+        """
+    )    
+
+    task1 >> task2 >> task3 >> task4 >> task5 >> task6 >> task7 >> task8 >> task9 >> task10 >> task11 >> task12 >> task13 >> task14
+    
--- Machine-learning/Readme.md
+++
@@ -1,9 +0,0 @@
-From the output can be explained that
-
-### Elbow method
-
-From the output above the elbow method show the best cluster number is 3
-
-### clustered Las Vegas
-
-
